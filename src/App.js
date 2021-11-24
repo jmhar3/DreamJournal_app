c3e19489e77cc427f2a2ab313b2e966df1777b7d
Jessica Harriman <jmhar@protonmail.com>
2021-11-24 12:14:28 +1100

    progress bars + shadows

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Home from './containers/home.js';
 import Login from './containers/login.js';
 import Dashboard from './containers/dashboard.js';
 import Nav from './containers/nav.js';
+import 'react-circular-progressbar/dist/styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/App.css';
 import './css/home.css';
 import './css/nav.css';
--- src/containers/dashboard.js
+++ src/containers/dashboard.js
@@ -1,3 +1,33 @@
+import { Link } from "react-router-dom";
+import { CircularProgressbar } from 'react-circular-progressbar';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import {Bar} from 'react-chartjs-2';
+import Chart from 'chart.js/auto'
+
+const expenseData = {
+    labels: ['Mon', 'Tue', 'Wed',
+             'Thu', 'Fri', 'Sat', 'Sun'],
+    datasets: [
+      {
+        label: 'Expense',
+        backgroundColor: '#d40819',
+        data: [65, 59, 80, 120, 56, 30, 10]
+      }
+    ]
+}
+
+const incomeData = {
+    labels: ['Mon', 'Tue', 'Wed',
+             'Thu', 'Fri', 'Sat', 'Sun'],
+    datasets: [
+      {
+        label: 'Income',
+        backgroundColor: '#d40819',
+        data: [650, 590, 300, 120, 89, 30, 120]
+      }
+    ]
+}
+
 const Dashboard = () => {
     const today = new Date()
     const curHr = today.getHours()
@@ -12,30 +42,61 @@ const Dashboard = () => {
         }
     }
 
+    const finance_goal = 66;
+
     return (
-      <div id="dashboard">
+      <main id="dashboard">
         <section id="dashboard-left">
             <h3>{ Date() }</h3>
-            <h1>{ greeting() }</h1>
-            <div id="finance">
-                <h1>Finance Charts</h1>
-            </div>
+            <h1>{ greeting() }, User Name</h1>
+            <section className="finance-charts">
+                <div>
+                    <h2>💸 Expense</h2>
+                    <p>$1200.00</p>
+                    <Bar data={expenseData} />
+                </div>
+                <div>
+                    <h2>🤑 Income</h2>
+                    <p>$1800.00</p>
+                    <Bar data={incomeData} />
+                </div>
+            </section>
             <div id="db-finance-goals">
-                <h1>Finance Goals</h1>
+                <div>
+                    <CircularProgressbar value={finance_goal} text={`🏖`} />
+                    <h4>Holiday</h4>
+                </div>
+                <div>
+                    <CircularProgressbar value={finance_goal} text={`📱`} />
+                    <h4>New Phone</h4>
+                </div>
+                <div>
+                    <CircularProgressbar value={finance_goal} text={`🏡`} />
+                    <h4>Home Deposit</h4>
+                </div>
             </div>
             <div id="upcoming-payments">
-                <h1>Upcoming Payments</h1>
+                <h2>Upcoming Payments</h2>
             </div>
         </section>
         <section id="dashboard-right">
             <div id="db-goals">
-                <h1>Goals</h1>
+                <span>
+                    <h2>Todays Focus</h2>
+                    <Link to="/newgoal" className="button">+</Link>
+                </span>
+                <ProgressBar>
+                    <ProgressBar variant="success" now={35} key={1} />
+                    <ProgressBar variant="warning" now={20} key={2} />
+                    <ProgressBar variant="danger" now={10} key={3} />
+                </ProgressBar>
+                <h2>Upcoming Goals</h2>
             </div>
             <div>
                 <h1>Notes</h1>
             </div>
         </section>
-      </div>
+      </main>
     )
   }
   
